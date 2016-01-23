650ddf9e10707d257f4ed9e9f199bf29ff476f0e
Shawn Mehan <shawn.mehan@ieee.org>
2016-01-22 23:00:20 -0800

    Added a time delay to requests to allow for variation of next request. Random int [0,1) * 60 seconds. Added a cookie to headers which works well if no variance in cookie data over total requests.

--- src/HTTPutils.py
+++ src/HTTPutils.py
@@ -10,6 +10,7 @@ import socket
 from stem import Signal
 from stem.control import Controller
 import random
+import time
 import http  # this seems to be needed for exception handling in http client
 from config import settings  # local config file
 
@@ -18,7 +19,8 @@ def define_headers(header_type):
     if header_type == 1:
         return({"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36",
                 "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-                "Accept-Language":"en-US,en;q=0.8"})
+                "Accept-Language":"en-US,en;q=0.8",
+                "Cookie":"FUID=1277301928; D_SID=66.214.195.230:9Oud/j/NKc695t+b15WXsOzXkfTDziv+jv7o46J8dE4; superCookie=cachedVersion%3Dtemplates-1.2.75%26cachedChannel%3Dyellbeta%26cachedFiles%3Dstyles%252Fcore.css%257Cmodules%252FPage%252Fhome%252Fstyles%252Fmain.css%257Ccore.js%257Cmodules%252FPage%252Fserp%252Fstyles%252Fmain.css%257Cmodules%252FPage%252Fbip%252Fstyles%252Fmain.css%26CookiePolicy%3D1; JSESSIONID=7C1D4B406B4A1D6ABD1CD3502039243E; s_sq=%5B%5BB%5D%5D; useMultimap=OFF; SearchHistory0=14535107512433426; ScraperTracking=7kTnT68TSJdY9ScuQKm3; SearchedSession=true; AUTOKW2=0pizza; AUTOLOC=united+kingdom; s_fid=63DA4D9E9DDBDAF2-3E880F8E669AABC6; s_cc=true; s_vi=[CS]v1|2B4F960685011479-600001316005B11F[CE]; SEARCH_LOC=United%20Kingdom-%3A-United%20Kingdom-%3A-%0A53.59622935-%3A--1.75987285%0A-%3A-%0ACountry%0A-%3A-UNITED%20KINGDOM; SEARCH_KEYWORDS=pizza; _ga=GA1.2.98864706.1453272078; RT=sl=4&ss=1453338560468&tt=76693&obo=0&sh=1453511170570%3D4%3A0%3A76693%2C1453510759897%3D3%3A0%3A50911%2C1453510013661%3D2%3A0%3A39028%2C1453508704658%3D1%3A0%3A14251&dm=yell.com&si=6b0fdd38-eb27-4087-a84e-97bf70676799&bcn=%2F%2F36f1f369.mpstat.us%2F&r=https%3A%2F%2Fwww.yell.com%2Fucs%2FUcsSearchAction.do%3F24005052db7a2c87c5d9de43dd418adb&ul=1453512809733&hd=1453512810364; D_PID=C2974EEC-C295-3B19-881B-3987D348B229; D_IID=2E8928B6-5A9A-3071-AB7B-67971A1A0D51; D_UID=39BAF301-FC9A-3D89-8A8C-24C73B90F925; D_HID=2+MzGliJiPfl395tghgZQXqG5SLvXkhHMUcARWuinRQ; NSC_mcw_xxx-b.zfmmhspvq.dpn_80=ffffffffc3a0421f45525d5f4f58455e445a4a421517"})
     else:
         return({"User-Agent":"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36",
                 "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
@@ -37,9 +39,16 @@ def rotate_ip():
             print("Failed to contact Tor controller")
 
 
+def immitate_user():
+    random.seed()
+    delay = random.random()*60
+    time.sleep(delay)
+
+
 def init_tor(header_type):
     try:
         rotate_ip()
+        immitate_user()
         socks.set_default_proxy(socks.SOCKS5, "localhost", 9050)
         socket.socket = socks.socksocket
         r = Request('http://icanhazip.com', headers=define_headers(header_type))
--- src/mainscrape.py
+++ src/mainscrape.py
@@ -8,27 +8,28 @@ from HTTPutils import *
 def get_details(entry):
     this_page, base_url = get_page(entry['url'], 1)
     try:
-        entries = this_page.find("div", {"id": "inspection-reports-main"}).find_all("p")
-        entry['URN'] = entries[0].find("strong").get_text()
-        entry['street'] = entries[1].contents[0]
-        entry['town'] = entries[1].contents[2]
-        entry['parish'] = entries[1].contents[4]
-        entry['postalcode'] = entries[1].contents[6]
-        entry['Phone'] = entries[2].contents[0]
+        entries = this_page.find("strong", {"itemprop": "telephone"}).get_text()
+        # entry['URN'] = entries[0].find("strong").get_text()
+        # entry['street'] = entries[1].contents[0]
+        # entry['town'] = entries[1].contents[2]
+        # entry['parish'] = entries[1].contents[4]
+        # entry['postalcode'] = entries[1].contents[6]
+        entry['Phone'] = entries
     except Exception as e:
         print("Failed to get details for %s" % entry['url'])
     return(entry)
 
 
+# page.find("div", {"class":"col-sm-24 businessCapsule businessCapsule-fle js-LocalBusiness"})
 def get_list(page, data, base_url):
-    entries = page.find("ul", {"class": "resultsList"}).find_all("li")
+    entries = page.find_all("div", {"class": "businessCapsule"})
     for e in entries:
         entry = {}
-        entry['name'] = e.find("a").get_text()
-        entry['url'] = ("".join((strip_final_slash(base_url),e.find("a").attrs['href'])))
-        entry['address'] = e.find("p").get_text()
-        entry['URN'] = e.find("p").find_next_sibling("p").get_text()
-        entry['Provider'] = e.find("p").find_next_sibling("p").find_next_sibling("p").get_text()
+        entry['name'] = e.find("div", {"class":"businessCapsule--title"}).h2.get_text()
+        entry['url'] = "".join((strip_final_slash(base_url),e.find("div", {"class":"businessCapsule--title"}).a.attrs['href']))
+        # entry['address'] = e.find("p").get_text()
+        # entry['URN'] = e.find("p").find_next_sibling("p").get_text()
+        # entry['Provider'] = e.find("p").find_next_sibling("p").find_next_sibling("p").get_text()
         entry = get_details(entry)
         data[entry['name']] = entry
     return(data)
@@ -38,7 +39,7 @@ def process_page(page, base_url, current_url, output, pc, recursion_limit):
     output = get_list(page, output, base_url)
     pc += 1  # page count up 1
     if pc <= recursion_limit:
-        next_url = "".join(("http://reports.ofsted.gov.uk/inspection-reports/find-inspection-report/results/1/any/any/any/any/any/any/rg42%201ya/20/any/0/0?page=",str(pc)))
+        next_url = "".join(("https://www.yell.com/ucs/UcsSearchAction.do?keywords=pizza&location=United+Kingdom&pageNum=",str(pc)))
         page, base_url = get_page(next_url, 1)
         output = process_page(page, base_url, next_url, output, pc, recursion_limit)
     return(output)
@@ -57,8 +58,7 @@ def process_output(data):
 def main():
     initial_url = "http://mugshots.louisvilleky.gov/archonixxjailsiteslmdc/archonixxjailpublic/"
     initial_url = "https://www.yell.com/ucs/UcsSearchAction.do?keywords=pizza&location=United+Kingdom&pageNum=8"
-    initial_url = "http://www.medspa.com/united-states?page=1&&reviews&sort=name%20ASC"
-    initial_url = "http://reports.ofsted.gov.uk/inspection-reports/find-inspection-report/results/1/any/any/any/any/any/any/rg42%201ya/20/any/0/0?page=0"
+    # TODO need to clean this up so that I can pass the initial_cookie and initial_url as part of the object init
     output = {}
     page, base_url = get_page(initial_url, 1)
     data = process_page(page, base_url, initial_url, output, 1, 2)
