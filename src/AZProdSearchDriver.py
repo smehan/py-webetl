eb5771b2cf932d6baa992f53cc4c048e30479c80
Shawn Mehan <shawn.mehan@ieee.org>
2016-02-17 16:43:00 -0800

    moved AZProdSearch class into a package. Removed old driver class as current design doesn't use it.

--- src/AZProdSearchDriver.py
+++
@@ -1,7 +0,0 @@
-from AZProdSearch import *
-from amazonproduct import API
-
-
-api = API(locale='us')
-root = api.item_lookup('0136042597', IdType='ISBN',
-                     SearchIndex='Books', ResponseGroup='Reviews', ReviewPage=1)
